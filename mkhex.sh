050fdb59ab370fd46d28a06f869ef29eeb938ca4
NoviceLive <rectigu@gmail.com>
2015-08-11 15:30:54 +0800

    Added the auto-generating notice.

--- mkhex.sh
+++ mkhex.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+#
+# I am a bash starter. Feel free to improve
+# or adapt this script.
+#
 
 #
 # Copyright 2015 Gu Zhengxiong <rectigu@gmail.com>
+#
+# GPL
 #
 
 
@@ -14,20 +20,42 @@ then
 
     if [[ $2 -eq 2 ]]
     then
+        printf '/*\n * This file was automatically generated'
+        printf ' by mkhex.sh,\n * '
+        printf 'which, together with the complete\n * '
+        printf 'and heavily commented assembly source code\n * '
+        printf 'for this shellcode,'
+        printf ' is available at\n * '
+        printf 'https://github.com/NoviceLive/shellcoding.\n * '
+        printf '\n * For those curious heads\n * '
+        printf "striving to figure out what's under the hood.\n"
+        printf ' *\n */\n\n\n'
         printf '# include <stdio.h>\n# include <string.h>\n\n\n'
         printf 'int\nmain(void)\n{\n  '
         printf 'char *shellcode = "'
     fi
+
+    c=0
     for i in $hex
     do
+        if [[ $2 -eq 2 ]] && [[ $c -eq 10 ]]
+        then
+            c=0
+            printf '"\n    "'
+        fi
+
         if [[ $2 -eq 1 ]]
         then
             fmt='%s'
         else
             fmt='\\x%s'
         fi
+
         printf $fmt "$i"
+
+        c=$(($c + 1))
     done
+
     if [[ $2 -eq 2 ]]
     then
         printf '";\n\n'
