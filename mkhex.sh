69227ee95a4bd564ec105ced0d696e8f2f46e670
NoviceLive <rectigu@gmail.com>
2015-08-11 19:16:03 +0800

    Added 3 for mprotect on Linux and 4 for VirtualProtect on Windows.

--- mkhex.sh
+++ mkhex.sh
@@ -18,7 +18,7 @@ if [[ $# -ge 1 ]]
 then
     hex=$(objdump -d "$1" | grep -oP $regex)
 
-    if [[ $2 -eq 2 ]]
+    if [[ $2 -ge 2 ]]
     then
         printf '/*\n * This file was automatically generated'
         printf ' by mkhex.sh,\n * '
@@ -30,7 +30,17 @@ then
         printf '\n * For those curious heads\n * '
         printf "striving to figure out what's under the hood.\n"
         printf ' *\n */\n\n\n'
-        printf '# include <stdio.h>\n# include <string.h>\n\n\n'
+        printf '# include <stdio.h>\n# include <string.h>\n'
+        if [[ $2 -eq 3 ]]
+        then
+            printf '# include <stdint.h>\n\n# include <sys/mman.h>\n'
+        fi
+
+        if [[ $2 -eq 4 ]]
+        then
+            printf '\n# include <windows.h>\n'
+        fi
+        printf '\n\n'
         printf 'int\nmain(void)\n{\n  '
         printf 'char *shellcode = "'
     fi
@@ -38,7 +48,7 @@ then
     c=0
     for i in $hex
     do
-        if [[ $2 -eq 2 ]] && [[ $c -eq 10 ]]
+        if [[ $2 -ge 2 ]] && [[ $c -eq 10 ]]
         then
             c=0
             printf '"\n    "'
@@ -56,9 +66,30 @@ then
         c=$(($c + 1))
     done
 
-    if [[ $2 -eq 2 ]]
+    if [[ $2 -ge 2 ]]
     then
         printf '";\n\n'
+        if [[ $2 -eq 3 ]]
+        then
+            printf '  int ret = mprotect ((void *)((uintptr_t)'
+            printf 'shellcode & ~4095), 4096,'
+            printf '\n    PROT_READ | PROT_WRITE | PROT_EXEC);\n\n'
+            printf '  if (ret) {\n'
+            printf '    printf ("mprotect\\n");\n'
+            printf '    return 1;\n  }\n\n'
+        fi
+
+        if [[ $2 -eq 4 ]]
+        then
+            printf '  DWORD why_must_this_variable;\n'
+            printf '  BOOL ret = VirtualProtect (shellcode,'
+            printf ' strlen(shellcode),'
+            printf '\n    PAGE_EXECUTE_READWRITE, '
+            printf '&why_must_this_variable);\n\n'
+            printf '  if (!ret) {\n'
+            printf '    printf ("VirtualProtect\\n");\n'
+            printf '    return 1;\n  }\n'
+        fi
         printf '  printf("strlen(shellcode)=%%d\\n"'
         printf ', strlen(shellcode));'
         printf '\n\n  ((void (*)(void))shellcode)();\n\n'
